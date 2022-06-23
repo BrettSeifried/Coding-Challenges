b72ff36d81d00a8d33bf54d4a5da1df1e5f61523
Brett Seifried <seifriedbrett@gmail.com>
2022-06-23 10:47:13 -0700

    completed O(n) twoNumberSum

--- challeneges/Two Number sum/twoNumberSum.js
+++ challeneges/Two Number sum/twoNumberSum.js
@@ -1,6 +1,22 @@
-// function twoNumberSum(array, targetSum) {
-//   // Write your code here.
-// }
+function twoNumberSum(array, targetSum) {
+  // Write your code here.
+  // set an empty array
+  const nums = {};
+  //checks each index of the array, no longer let i = 0, i < arr.length .....
+  for (const num of array) {
+    // takes target, subtracts from each i. targetSum - i = Y
+    const matching = targetSum - num;
+    if (matching in nums) {
+      // if Y === a number in the array, return
+      return [matching, num];
+    } else {
+      //Store in nums if doesn't match targetSum
+      nums[num] = true;
+    }
+  }
+  return [];
+}
+console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10));
 
-// // Do not edit the line below.
-// exports.twoNumberSum = twoNumberSum;
+// Do not edit the line below.
+exports.twoNumberSum = twoNumberSum;
--- challeneges/Two Number sum/twoNumberSum.test.js
+++ challeneges/Two Number sum/twoNumberSum.test.js
@@ -1,9 +1,7 @@
-// const program = require('./program');
-// const chai = require('chai');
+const { twoNumberSum } = require('./twoNumberSum');
 
-// it('Test Case #1', () => {
-//   const output = program.twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10);
-//   chai.expect(output.length === 2).to.deep.equal(true);
-//   chai.expect(output.includes(11)).to.deep.equal(true);
-//   chai.expect(output.includes(-1)).to.deep.equal(true);
-// });
+test('Test Case #1', (expect) => {
+  const expected = [-1, 11];
+  const actual = twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10);
+  expect.deepEqual(actual, expected);
+});
