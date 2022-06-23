6147f503e81f132effc8f451df27b1acbc170914
Brett Seifried <seifriedbrett@gmail.com>
2022-06-23 10:00:24 -0700

    initial commit

---
+++ challeneges/Two Number sum/twoNumberSum.test.js
@@ -0,0 +1,9 @@
+// const program = require('./program');
+// const chai = require('chai');
+
+// it('Test Case #1', () => {
+//   const output = program.twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10);
+//   chai.expect(output.length === 2).to.deep.equal(true);
+//   chai.expect(output.includes(11)).to.deep.equal(true);
+//   chai.expect(output.includes(-1)).to.deep.equal(true);
+// });
