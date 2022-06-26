978785de0cefccfd0807334c4328d7bbdb29af4f
Brett Seifried <seifriedbrett@gmail.com>
2022-06-26 09:34:59 -0700

    Fibonacci completed

--- script.js
+++ script.js
@@ -348,3 +348,15 @@ function removeDuplicatesFromLinkedList(linkedList) {
 }
 
 console.log(removeDuplicatesFromLinkedList());
+
+//Fibonacci sequence ---------------------
+function getNthFib(n) {
+  if (n === 2) {
+    return 1;
+  } else if (n === 1) {
+    return 0;
+  } else {
+    return getNthFib(n - 1) + getNthFib(n - 2);
+  }
+}
+console.log(getNthFib(6));
