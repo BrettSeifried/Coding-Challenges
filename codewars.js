1d2bff430eb7d0cfc7ffc0bf2a907b55b773a750
Brett Seifried <seifriedbrett@gmail.com>
2022-07-25 14:29:28 -0700

    Return Positives

--- codewars.js
+++ codewars.js
@@ -20,3 +20,18 @@ function getCount(str) {
 // });
 
 //--------------------------------
+// Return Positives
+function positiveSum(arr) {
+    let result = 0;
+    
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] >= 0) {
+        result += arr[i]
+      }
+    }
+    return result;
+  }
+
+  // Example [1,-4,7,12] => 1 + 7 + 12 = 20
+//------------------------
+
