a37f90646dfb5c6e52ed90c2ec8bed4a34e12ee7
Brett Seifried <seifriedbrett@gmail.com>
2022-07-28 15:05:03 -0700

    fixed a line

--- codewars.js
+++ codewars.js
@@ -122,7 +122,7 @@ function paperwork(n, m) {
 function solution(number){
   var sum = 0;
   
-  for(var i = 1;i< number; i++){
+  for(let i = 1;i< number; i++){
     if(i % 3 == 0 || i % 5 == 0){
       sum += i
     }
